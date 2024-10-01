dca830ddc5ca878f32d2e100a47e681f33d2a942
annieradams <adams.annie11@gmail.com>
2024-10-01 10:18:17 -0700

    updated tests to match dataframes for task 2

---
+++ dist-task2/autograder/tests/q3_c.py
@@ -0,0 +1,9 @@
+OK_FORMAT = True
+
+test = {   'name': 'q3_c',
+    'points': 1,
+    'suites': [   {   'cases': [{'code': '>>> assert coral_div_types.equals(coral_div.dtypes), "The data types of the columns do not match."\n>>> \n>>> \n', 'hidden': True, 'locked': False}],
+                      'scored': True,
+                      'setup': '',
+                      'teardown': '',
+                      'type': 'doctest'}]}
---
+++ dist-task2/autograder/tests/q8.py
@@ -0,0 +1,9 @@
+OK_FORMAT = True
+
+test = {   'name': 'q8',
+    'points': 1,
+    'suites': [   {   'cases': [{'code': ">>> assert pd.read_csv('data/q8_df.csv',  index_col=0).equals(coral_cover)\n", 'hidden': True, 'locked': False}],
+                      'scored': True,
+                      'setup': '',
+                      'teardown': '',
+                      'type': 'doctest'}]}
