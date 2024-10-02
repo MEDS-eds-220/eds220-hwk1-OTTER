7d77facf31aba9e5fd606f49916837912df28d4d
annieradams <adams.annie11@gmail.com>
2024-10-02 10:25:21 -0700

    final edits for task 2 and 3

--- dist-task2/autograder/tests/q14.py
+++ dist-task2/autograder/tests/q14.py
@@ -2,7 +2,7 @@ OK_FORMAT = True
 
 test = {   'name': 'q14',
     'points': 1,
-    'suites': [   {   'cases': [{'code': ">>> assert pd.read_csv('data/q14_df.csv',  index_col=0).equals(subset_na)\n", 'hidden': True, 'locked': False}],
+    'suites': [   {   'cases': [{'code': ">>> assert pd.read_csv('data/q14_df.csv', index_col=0).equals(subset_na)\n", 'hidden': True, 'locked': False}],
                       'scored': True,
                       'setup': '',
                       'teardown': '',
--- dist-task2/autograder/tests/q9.py
+++ dist-task2/autograder/tests/q9.py
@@ -2,7 +2,7 @@ OK_FORMAT = True
 
 test = {   'name': 'q9',
     'points': 1,
-    'suites': [   {   'cases': [{'code': ">>> assert pd.read_csv('data/q9_df.csv',  index_col=0).equals(filtered_data)\n", 'hidden': True, 'locked': False}],
+    'suites': [   {   'cases': [{'code': ">>> assert pd.read_csv('data/q9_df.csv', index_col=0).equals(filtered_data)\n", 'hidden': True, 'locked': False}],
                       'scored': True,
                       'setup': '',
                       'teardown': '',
